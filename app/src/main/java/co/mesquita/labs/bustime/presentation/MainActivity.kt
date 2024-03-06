b8133a931cb3df6d1ff85270d665c4ddf529897b
Mesquita <mesquita@aospa.co>
2024-03-06 13:54:22 -0300

    MainActivity: Implement input method
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- app/src/main/java/co/mesquita/labs/bustime/presentation/MainActivity.kt
+++ app/src/main/java/co/mesquita/labs/bustime/presentation/MainActivity.kt
@@ -6,9 +6,14 @@
 
 package co.mesquita.labs.bustime.presentation
 
+import android.app.RemoteInput
+import android.content.Intent
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -18,6 +23,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -29,6 +38,8 @@ import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
+import androidx.wear.input.RemoteInputIntentHelper
+import androidx.wear.input.wearableExtender
 import co.mesquita.labs.bustime.R
 import co.mesquita.labs.bustime.presentation.theme.BusTimeGoianiaTheme
 
@@ -58,9 +69,7 @@ fun WearApp() {
             TimeText()
             Column {
                 Greeting()
-                Button(
-                    text = "Buscar"
-                )
+                Button()
             }
 
         }
@@ -80,18 +89,42 @@ fun Greeting() {
 }
 
 @Composable
-fun Button(
-    text: String
-) {
+fun Button() {
+    val defaultText = stringResource(R.string.edit_user_input)
+    var userInput by remember { mutableStateOf(defaultText) }
+    val inputTextKey = "input_text"
+    val remoteInputs: List<RemoteInput> = listOf(
+        RemoteInput.Builder(inputTextKey)
+            .setLabel(stringResource(id = R.string.edit_user_input))
+            .wearableExtender {
+                setEmojisAllowed(false)
+                setInputActionType(EditorInfo.IME_ACTION_DONE)
+            }.build()
+    )
+
+    val launcher = rememberLauncherForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        it.data?.let { data ->
+            val results: Bundle = RemoteInput.getResultsFromIntent(data)
+            val newInputText: CharSequence? = results.getCharSequence(inputTextKey)
+            userInput = newInputText?.toString() ?: ""
+        }
+    }
+
+    val intent: Intent = RemoteInputIntentHelper.createActionRemoteInputIntent()
+    RemoteInputIntentHelper.putRemoteInputsExtra(intent, remoteInputs)
+
     Button(
         onClick = {
+            launcher.launch(intent)
         },
         modifier = Modifier
             .fillMaxWidth()
             .size(50.dp)
             .padding(start = 30.dp, end = 30.dp),
     ) {
-        Text(text)
+        Text(stringResource(id = R.string.search_button))
     }
 }
 
