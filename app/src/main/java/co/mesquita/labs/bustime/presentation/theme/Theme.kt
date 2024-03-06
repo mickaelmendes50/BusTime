0042956f171c21d13b722b3c504be1b6c192e269
Mesquita <mesquita@aospa.co>
2024-03-06 10:10:35 -0300

    Initial commit

---
+++ app/src/main/java/co/mesquita/labs/bustime/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package co.mesquita.labs.bustime.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun BusTimeGoiâniaTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
