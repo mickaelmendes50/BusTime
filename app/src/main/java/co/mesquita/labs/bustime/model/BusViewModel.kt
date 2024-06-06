5329d6e6dcb2596e1cbc7d7c70d4d06bbddcbe0a
Mesquita <mesquita@aospa.co>
2024-06-06 16:57:21 -0300

    model: Move the valid stopId function to model
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- app/src/main/java/co/mesquita/labs/bustime/model/BusViewModel.kt
+++ app/src/main/java/co/mesquita/labs/bustime/model/BusViewModel.kt
@@ -1,14 +1,21 @@
 package co.mesquita.labs.bustime.model
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavController
+import co.mesquita.labs.bustime.api.Endpoints
 import co.mesquita.labs.bustime.repository.BusRepository
+import co.mesquita.labs.bustime.util.NetworkUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,6 +23,27 @@ class BusViewModel @Inject constructor(
     private val busRepository: BusRepository
 ): ViewModel() {
     val isLoading = mutableStateOf(false)
+
+    fun isStopIdValid(stopId: String): LiveData<Boolean> {
+        val retrofitClient = NetworkUtils.getRetrofitInstance()
+        val service = retrofitClient.create(Endpoints::class.java)
+        val resultLiveData = MutableLiveData<Boolean>()
+        isLoading.value = true
+
+        viewModelScope.launch(Dispatchers.IO) {
+            val response = service.isStopIdValid(stopId)
+            try {
+                if (response.isSuccessful) {
+                    val status = response.body()?.get("status")?.asString
+                    resultLiveData.postValue(status == "sucesso")
+                }
+            } catch (e: HttpException) {
+                Log.e("Retrofit", "Exception ${e.message}")
+            }
+            isLoading.value = false
+        }
+        return resultLiveData
+    }
 
     fun getBussTime(busStop: String): LiveData<String> {
         isLoading.value = true
--- app/src/main/java/co/mesquita/labs/bustime/presentation/MainActivity.kt
+++ app/src/main/java/co/mesquita/labs/bustime/presentation/MainActivity.kt
@@ -1,7 +1,6 @@
 package co.mesquita.labs.bustime.presentation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -9,27 +8,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.wear.compose.navigation.SwipeDismissableNavHost
 import androidx.wear.compose.navigation.composable
 import androidx.wear.compose.navigation.rememberSwipeDismissableNavController
-import co.mesquita.labs.bustime.api.Endpoints
 import co.mesquita.labs.bustime.model.BusViewModel
 import co.mesquita.labs.bustime.presentation.theme.BusListScreen
 import co.mesquita.labs.bustime.presentation.theme.BusTimeGoianiaTheme
 import co.mesquita.labs.bustime.presentation.theme.HomeScreen
 import co.mesquita.labs.bustime.presentation.theme.NotFoundScreen
 import co.mesquita.labs.bustime.presentation.theme.SearchScreen
-import co.mesquita.labs.bustime.util.NetworkUtils
 import com.google.android.horologist.compose.layout.AppScaffold
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
-import retrofit2.HttpException
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -55,29 +47,10 @@ class MainActivity : ComponentActivity() {
         return title.text().substringAfterLast("Ponto ID:")
     }
 
-    private fun isStopIdValid(navController: NavController, stopId: String) {
-        val retrofitClient = NetworkUtils.getRetrofitInstance()
-        val service = retrofitClient.create(Endpoints::class.java)
-
-        lifecycleScope.launch {
-            val response = service.isStopIdValid(stopId)
-            withContext(Dispatchers.Main) {
-                try {
-                    if (response.isSuccessful) {
-                        val status = response.body()?.get("status")?.asString
-                        if (status != "sucesso") {
-                            navController.navigate("notFound")
-                        }
-                    }
-                } catch (e: HttpException) {
-                    Log.e("Retrofit", "Exception ${e.message}")
-                }
-            }
-        }
-    }
-
     private fun onSearchButtonClick(navController: NavController, stopNumber: String) {
-        isStopIdValid(navController, stopNumber)
+        this.viewModel.isStopIdValid(stopNumber).observe(this) {
+            isValid -> if (!isValid) navController.navigate("notFound")
+        }
 
         this.viewModel.getBussTime(stopNumber).observe(this) {
             htmlContent.value = it
