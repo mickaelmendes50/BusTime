6b86853537ba5f2353e0b924a85b646dcd60322d
Mesquita <mesquita@aospa.co>
2024-06-07 14:22:14 -0300

    dependencies: Add support to Google Maps API
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,6 +84,7 @@ dependencies {
 
     // Google
     implementation("com.google.android.gms:play-services-wearable:18.2.0")
+    implementation("com.google.maps.android:maps-compose:5.0.3")
     implementation("com.google.code.gson:gson:2.11.0")
 
     implementation("androidx.core:core-splashscreen:1.0.1")
