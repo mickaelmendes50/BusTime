f2cec8818224d335eaf38dd6ccbdf188e789cc8d
Mickael Mesquita <mesquita@aospa.co>
2026-02-20 14:40:09 -0300

    gradle: Updated test dependencies
    Signed-off-by: Mickael Mesquita <mesquita@aospa.co>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,9 +96,9 @@ dependencies {
 
     // Testing
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.7")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.6.7")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.10.3")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.10.3")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
 }
