9e7c143a02c59e3de76fb97fca827a21996f7e31
Mickael Mesquita <mesquita@aospa.co>
2026-02-21 11:30:37 -0300

    screens: NotFoundScreen: Convert the advise into a component
    Signed-off-by: Mickael Mesquita <mesquita@aospa.co>

--- app/src/main/java/co/mesquita/labs/bustime/ui/screens/NotFoundScreen.kt
+++ app/src/main/java/co/mesquita/labs/bustime/ui/screens/NotFoundScreen.kt
@@ -2,24 +2,14 @@ package co.mesquita.labs.bustime.ui.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.WarningAmber
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.MaterialTheme
-import androidx.wear.compose.material.Text
 import co.mesquita.labs.bustime.R
+import co.mesquita.labs.bustime.ui.components.EmptyFragment
 
 @Composable
 fun NotFoundScreen() {
@@ -29,24 +19,6 @@ fun NotFoundScreen() {
             .background(MaterialTheme.colors.background),
         contentAlignment = Alignment.Center
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Icon(
-                imageVector = Icons.Outlined.WarningAmber,
-                contentDescription = null,
-                modifier = Modifier.size(45.dp),
-                tint = MaterialTheme.colors.primary,
-            )
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(5.dp),
-                textAlign = TextAlign.Center,
-                fontSize = 15.sp,
-                color = MaterialTheme.colors.primary,
-                text = stringResource(R.string.empty_table)
-            )
-        }
+        EmptyFragment(stringResource(R.string.empty_table))
     }
 }
