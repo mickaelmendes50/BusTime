867492a29bd410018dbaf9dca08708e86b428384
Mickael Mesquita <mesquita@aospa.co>
2026-02-21 11:07:09 -0300

    app: Bump version to 1.1
    Signed-off-by: Mickael Mesquita <mesquita@aospa.co>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "co.mesquita.labs.bustime"
         minSdk = 30
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         vectorDrawables {
             useSupportLibrary = true
         }
